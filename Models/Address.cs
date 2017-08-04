acdedf4e4bb8a996fa941cd4f3f4612417a21845
Robert Murray <alexmurray93@hotmail.com>
2017-08-04 10:27:30 -0700

    Add setup code, create Address and Contact classes with getter and setter methods, and create README file.

---
+++ Models/Address.cs
@@ -0,0 +1,43 @@
+namespace AddressBook
+{
+  public class Address
+  {
+    private string _street;
+    private string _city;
+    private string _state;
+
+    public Address(string theStreet, string theCity, string theState)
+    {
+      _street = theStreet;
+      _city = theCity;
+      _state = theState;
+    }
+
+    public string GetStreet()
+    {
+      return _street;
+    }
+    public void SetStreet(string newStreet)
+    {
+      _street = newStreet;
+    }
+
+    public string GetCity()
+    {
+      return _city;
+    }
+    public void SetCity(string newCity)
+    {
+      _city = newCity;
+    }
+
+    public string GetState()
+    {
+      return _state;
+    }
+    public void SetState(string newState)
+    {
+      _state = newState;
+    }
+  }
+}
---
+++ Models/Contacts.cs
@@ -0,0 +1,41 @@
+namespace AddressBook.Models
+{
+  public class Contact
+  {
+    private string _name;
+    private string _phone;
+    private Address _home;
+    private static List<Contact> _contacts = new List<Contact> {};
+
+    public Contact(string theName, string thePhone, string theHome)
+    {
+      _name = theName;
+      _phone = thePhone;
+      _home = theHome;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+
+    public string GetPhone()
+    {
+      return _phone;
+    }
+    public void SetPhone(string newPhone)
+    {
+      _phone = newPhone;
+    }
+
+    public static List<Contact> GetBook()
+    {
+      return _contacts;
+    }
+
+  }
+}
